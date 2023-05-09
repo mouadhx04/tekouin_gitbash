8604d98490419e89fa0ddf1e2cefb52220fb4185
mouadhx04 <85496711+mouadhx04@users.noreply.github.com>
2023-05-09 03:15:08 +0200

    nb dirs and subdirs

--- task_5.bash
+++ task_5.bash
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# Initialize the count variable to 0
 count=0
-
-# Loop over all subdirectories (including hidden directories) in the current directory
 for dir in $(find . -type d -not -path '*/\.*' -not -path '.' -not -path '..'); do
-    # Increment the count variable for each subdirectory
     count=$((count + 1))
 done
-
-# Print the final count
 echo "The number of directories and sub-directories in the current directory is: $count"
